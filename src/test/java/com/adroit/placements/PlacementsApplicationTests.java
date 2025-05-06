c125d10ba30dd857df3cd5c53f04842eff47f1d9
SasiKDP <sasank9110@gmail.com>
2025-05-06 14:40:30 +0530

    Initial commit with Spring Boot microservice project

---
+++ src/test/java/com/adroit/placements/PlacementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adroit.placements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
