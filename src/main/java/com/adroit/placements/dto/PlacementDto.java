8c4603198937776c73d2c9ae5c069966a920f21f
vadaparthivinuthna <vadaparthivinuthna@gmail.com>
2025-06-16 16:58:19 +0530

    changed the files by getter and setter methods ,added ip address

--- src/main/java/com/adroit/placements/dto/PlacementDto.java
+++ src/main/java/com/adroit/placements/dto/PlacementDto.java
@@ -1,22 +1,18 @@
 package com.adroit.placements.dto;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.Email;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlacementDto {
 
     private String consultantName;
-
-    @Email(message = "Invalid email format")
     private String emailId;
     private LocalDate createdAt;
     private String technology;
@@ -28,24 +24,8 @@ public class PlacementDto {
     private String closedMonth;
     private String employementType;
     private String visa;
-
-    @DecimalMin(value = "0.0", inclusive = false, message = "Bill Rate From Client must be positive")
-    @Digits(integer = 10, fraction = 5, message = "Invalid format for Bill Rate From Client")
     private BigDecimal billRateFromClient;
-
-    @DecimalMin(value = "0.0", inclusive = false, message = "Bill Rate To Candidate must be positive")
-    @Digits(integer = 10, fraction = 5, message = "Invalid format for Bill Rate To Candidate")
     private BigDecimal billRateToCandidate;
-
-    @DecimalMin(value = "0.0", inclusive = false, message = "Pay Rate must be positive")
-    @Digits(integer = 10, fraction = 5, message = "Invalid format for Pay Rate")
     private BigDecimal payRate;
-
-    @DecimalMin(value = "0.0", inclusive = true, message = "Margin must be non-negative")
-    @Digits(integer = 10, fraction = 5, message = "Invalid format for Margin")
     private BigDecimal margin;
-
-
 }
-
-
